f7f3ceffdb96aafea71c512a9ccef1fdcff59e35
LuteceR <93578660+LuteceR@users.noreply.github.com>
2025-09-08 13:49:01 +0000

    k

---
+++ ATPRV/PT_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Studying 
+{
+    class Program
+    {
+        static void FunctionMethod1(int N)
+        {
+            var stopwatch = new Stopwatch();
+            double[,] a = new double[N, N];
+            stopwatch.Start();
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    a[i, j] = i / (j + 1);
+            stopwatch.Stop();
+            Console.WriteLine($"Время создания {stopwatch.ElapsedMilliseconds} мс");
+        }
+        public static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            FunctionMethod1(10);
+        }
+    }
+}
