c85dba33e338d27b22d6bfaae06b5d3f8e60e431
LuteceR <93578660+LuteceR@users.noreply.github.com>
2025-09-08 13:55:21 +0000

    aasd

--- ATPRV/PT_1/Program.cs
+++ ATPRV/PT_1/Program.cs
@@ -6,22 +6,29 @@ namespace Studying
 {
     class Program
     {
-        static void FunctionMethod1(int N)
+        static long FunctionMethod1(int M, int N)
         {
-            var stopwatch = new Stopwatch();
-            double[,] a = new double[N, N];
-            stopwatch.Start();
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++)
-                    a[i, j] = i / (j + 1);
-            stopwatch.Stop();
-            Console.WriteLine($"Время создания {stopwatch.ElapsedMilliseconds} мс");
+            for (int i = 0; i < M; i++)
+            {
+                var stopwatch = new Stopwatch();
+                double[,] a = new double[N, N];
+                stopwatch.Start();
+                for (int i = 0; i < N; i++)
+                    for (int j = 0; j < N; j++)
+                        a[i, j] = i / (j + 1);
+                stopwatch.Stop();
+                //Console.WriteLine($"Время создания {stopwatch.ElapsedMilliseconds} мс");
+                return stopwatch.ElapsedMilliseconds;
+            }
+            ;
         }
+
+
         public static void Main(string[] args)
         {
             var stopwatch = new Stopwatch();
 
-            FunctionMethod1(10);
+            _ = FunctionMethod1(100, 10000);
         }
     }
 }
