6506190d86ed1c957e22dd363b54e3a847641f54
LuteceR <93578660+LuteceR@users.noreply.github.com>
2025-09-08 21:04:11 +0500

    Update Program.cs

--- ATPRV/PT_1/Program.cs
+++ ATPRV/PT_1/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Threading;
 
-namespace Studying 
+namespace c__project_for_studying
 {
-    class Program
+    public class MyProgram
     {
-        static long FunctionMethod1(int N)
+        const int N = 10000;
+        public static double Method1()
         {
             var stopwatch = new Stopwatch();
             double[,] a = new double[N, N];
@@ -15,16 +16,65 @@ namespace Studying
                 for (int j = 0; j < N; j++)
                     a[i, j] = i / (j + 1);
             stopwatch.Stop();
-            //Console.WriteLine($"Время создания {stopwatch.ElapsedMilliseconds} мс");
-            return stopwatch.ElapsedMilliseconds;
+            return stopwatch.Elapsed.TotalSeconds;
         }
 
-
-        public static void Main(string[] args)
+        public static double Method2()
         {
             var stopwatch = new Stopwatch();
-            long[] arrfrst = new long[100];
-            _ = FunctionMethod1(10000);
+            double[,] a = new double[N, N];
+            stopwatch.Start();
+            for (int j = 0; j < N; j++)
+                for (int i = 0; i < N; i++)
+                    a[i, j] = i / (j + 1);
+            stopwatch.Stop();
+            return stopwatch.Elapsed.TotalSeconds;
+        }
+
+        public static double Method3()
+        {
+            var stopwatch = new Stopwatch();
+            double[,] a = new double[N, N];
+            stopwatch.Start();
+            for (int i = N - 1; i >= 0; i--)
+                for (int j = N - 1; j >= 0; j--)
+                    a[i, j] = i / (j + 1);
+            stopwatch.Stop();
+            return stopwatch.Elapsed.TotalSeconds;
+        }
+
+        public static double Method4()
+        {
+            var stopwatch = new Stopwatch();
+            double[,] a = new double[N, N];
+            stopwatch.Start();
+            for (int j = N - 1; j >= 0; j--)
+                for (int i = N - 1; i >= 0; i--)
+                    a[i, j] = i / (j + 1);
+            stopwatch.Stop();
+            return stopwatch.Elapsed.TotalSeconds;
+        }
+
+        public static void Main()
+        {
+            // массивы с результатами
+            double[] method1 = new double[100];
+            double[] method2 = new double[100];
+            double[] method3 = new double[100];
+            double[] method4 = new double[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+                method1[i] = Method1();
+                method2[i] = Method2();
+                method3[i] = Method3();
+                method4[i] = Method4();
+            }
+
+            Console.WriteLine($"Method1 min={method1.Min()}, max={method1.Max()}, average={method1.Average()}");
+            Console.WriteLine($"Method2 min={method2.Min()}, max={method2.Max()}, average={method2.Average()}");
+            Console.WriteLine($"Method3 min={method3.Min()}, max={method3.Max()}, average={method3.Average()}");
+            Console.WriteLine($"Method4 min={method4.Min()}, max={method4.Max()}, average={method4.Average()}");
         }
     }
 }
